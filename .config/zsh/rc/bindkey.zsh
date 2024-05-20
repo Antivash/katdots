4fb5de39d5bdacdd0175c447bf11bffcf26ac953
antivash <sterling.r.j.s@gmail.com>
2024-05-20 04:01:34 -0500

    Update bindkey.zsh

--- .config/zsh/rc/bindkey.zsh
+++ .config/zsh/rc/bindkey.zsh
@@ -41,9 +41,9 @@ bindkey '^[[1;5C' forward-word
 bindkey '^[[1;5D' backward-word
 
 ## history ##
-autoload -U history-search-end
-zle -N history-beginning-search-backward-end history-search-end
-zle -N history-beginning-search-forward-end history-search-end
+#autoload -U history-search-end
+#zle -N history-beginning-search-backward-end history-search-end
+#zle -N history-beginning-search-forward-end history-search-end
 # broken with auto-suggestion
 #bindkey '^P' history-beginning-search-backward-end
 #bindkey '^N' history-beginning-search-forward-end
@@ -56,24 +56,24 @@ zle -N history-beginning-search-forward-end history-search-end
 # history incremental search
 #bindkey "^R" history-incremental-search-backward
 #bindkey "^S" history-incremental-search-forward
-bindkey '^R' history-incremental-pattern-search-backward
-bindkey '^S' history-incremental-pattern-search-forward
-autoload -Uz smart-insert-last-word
-# Words of length 2 or more containing at least one of [a-zA-Z], /, or \
-	zstyle :insert-last-word match '*([[:alpha:]/\\]?|?[[:alpha:]/\\])*'
-zle -N insert-last-word smart-insert-last-word
-function _insert-last-word() { smart-insert-last-word; ARG=-2 }
-zle -N _insert-last-word
-bindkey '^]' _insert-last-word
-function insert-next-word() { zle insert-last-word -- 1 -1; ARG=-2 }
-zle -N insert-next-word
-bindkey '^_' insert-next-word
-function zle-line-finish() { ARG=-2 }
-zle -N zle-line-finish
-function insert-prev-arg() { zle insert-last-word -- 0 ${ARG:-2}; ARG=$(($ARG-1)) }
-zle -N insert-prev-arg
-bindkey '^^' insert-prev-arg
-bindkey '\e#' pound-insert
+#bindkey '^R' history-incremental-pattern-search-backward
+#bindkey '^S' history-incremental-pattern-search-forward
+#autoload -Uz smart-insert-last-word
+## Words of length 2 or more containing at least one of [a-zA-Z], /, or \
+#	zstyle :insert-last-word match '*([[:alpha:]/\\]?|?[[:alpha:]/\\])*'
+#zle -N insert-last-word smart-insert-last-word
+#function _insert-last-word() { smart-insert-last-word; ARG=-2 }
+#zle -N _insert-last-word
+#bindkey '^]' _insert-last-word
+#function insert-next-word() { zle insert-last-word -- 1 -1; ARG=-2 }
+#zle -N insert-next-word
+#bindkey '^_' insert-next-word
+#function zle-line-finish() { ARG=-2 }
+#zle -N zle-line-finish
+#function insert-prev-arg() { zle insert-last-word -- 0 ${ARG:-2}; ARG=$(($ARG-1)) }
+#zle -N insert-prev-arg
+#bindkey '^^' insert-prev-arg
+#bindkey '\e#' pound-insert
 
 ## cd ##
 function cd-up { zle push-line && LBUFFER='builtin cd ..' && zle accept-line }
